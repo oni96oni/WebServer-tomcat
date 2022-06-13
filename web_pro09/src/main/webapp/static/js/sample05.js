f1718a14fc85961ed7383a851968d30eb15457a0
oni <rainbow2774@naver.com>
2022-06-13 21:55:57 +0900

    javascript event

--- web_pro09/src/main/webapp/static/js/sample05.js
+++ web_pro09/src/main/webapp/static/js/sample05.js
@@ -1,6 +1,30 @@
 /**
  * 
  */
+function f1() {
+	var id1 = document.getElementById("id1");
+	var first_li = id1.firstElementChild;
+	var last_li = id1.lastElementChild;
+	first_li.style.color = "red";
+	last_li.style.color = "blue";
+} 
+
+function f2() {
+	var id2 = document.getElementById("id2");
+	var prev_li = id2.previousElementSibling;
+	var next_li = id2.nextElementSibling;
+	
+	prev_li.style.color = "red";
+	next_li.style.color = "blue";
+}
+
+function f3() {
+	var id3 = document.getElementById("id3");
+	var ul = id3.parentElement;
+	
+	ul.style.backgroundColor = "whitesmoke";
+}
+
 function addUrlList() {
 	var num = document.getElementById("id_num")
 	var url = document.getElementById("id_url");
---
+++ web_pro09/src/main/webapp/static/js/sample06.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+function f1() {
+	var id1 = document.getElementById("id1");
+	id1.innerHTML = "클릭 이벤트 동작!!!";
+}
+
+function f2(x) {
+	x.innerText = "클릭 이벤트 동작!!";
+}
